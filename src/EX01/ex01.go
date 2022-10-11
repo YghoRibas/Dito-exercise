052679cf5f87aa237fb2f6d92914c351c291a1b7
YghoRibas <yghor.dev@gmail.com>
2022-10-11 07:08:51 -0300

    create isSubsequence function

--- src/EX01/ex01.go
+++ src/EX01/ex01.go
@@ -1,1 +1,21 @@
+package EX01
 
+func IsSubsequence(listP []int, listS []int) bool {
+	listPSize := len(listP)
+	listSSize := len(listS)
+	var aux []int
+
+	if listSSize > listPSize {
+		return false
+	}
+
+	for i := 0; i < listSSize; i++ {
+		for j := 0 + i; j < listPSize; j++ {
+			if listP[j] == listS[i] {
+				aux[i] = listS[i]
+			}
+		}
+	} 
+
+	return aux[] == listS
+}
---
+++ src/main.go
@@ -0,0 +1,12 @@
+package main
+
+import(
+	"fmt"
+	"context"
+
+	"github.com/YghoRibas/Dito-exercise/EX01"
+)
+
+func main() {
+	fmt.Println(IsSubsequence([1,2,3,4,5],[2,5,4]))
+}
