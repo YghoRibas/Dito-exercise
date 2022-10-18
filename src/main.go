8f486583d1b504ff3222fc390e929146a7a669f9
YghoRibas <yghor.dev@gmail.com>
2022-10-18 07:51:51 -0300

    add comments

--- src/main.go
+++ src/main.go
@@ -7,8 +7,8 @@ import(
 )
 
 func main() {
-	var p = []int {1,2,4,6}
-	var s = []int {1,4,5}
+	var p = []int {1,2,4,6,3}
+	var s = []int {1,4,3}
 
 	showResult := EX01.IsSubsequence(p,s)
 	fmt.Println("result:", showResult.ItsSubsequence, "    out of array sum:", showResult.SumOutOfPrincipal)
